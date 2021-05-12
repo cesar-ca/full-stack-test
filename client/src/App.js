5c4a3e410a02dbbd7050c0aebcfc8398d2e2a038
Emanuel Castro <emanuel@minerva.kgi.edu>
2021-05-11 19:38:37 -0700

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,57 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+
+import './App.scss';
+
+import {AuthContextProvider, AuthProtectedRoute} from './AuthContext';
+import Header from './Header';
+import Home from './Home';
+import Login from './Login';
+import Passwords from './Passwords';
+import Register from './Register';
+import SectionItems from './SectionItems';
+import Components from './Components';
+import Sections from './Sections/Sections';
+import SectionsList from './Sections/SectionsList';
+
+function App() {
+  return (
+    <AuthContextProvider>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/passwords">
+            <Passwords />
+          </Route>
+
+          <Route path="/components">
+            <Components />
+          </Route>
+          <Route path="/sections">
+            <Sections />
+          </Route>
+          
+          {process.env.REACT_APP_FEATURE_REGISTRATION === 'true' && (
+            <Route path="/register">
+              <Register />
+            </Route>
+          )}
+          <AuthProtectedRoute path="/sectionItems">
+            <SectionItems />
+          </AuthProtectedRoute>
+        </Switch>
+      </Router>
+    </AuthContextProvider>
+  );
+}
+
+export default App;
---
+++ client/src/Countdown.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+function Countdown({seconds}){
+    const [secondsRemaining, setSecondsRemaining] = useState(0);
+
+    function onStart(){
+    const startTime = new Date();
+    setSecondsRemaining(seconds);
+    const intervalId = setInterval(function(){
+        const now = new Date();
+        const timeElapsed = now.getTime()-startTime.getTime();
+        const timeRemaining = Math.round(seconds- timeElapsed/1000);
+        if(timeRemaining<=0){
+            clearInterval(intervalId);
+            setSecondsRemaining(0);
+        }else{
+            setSecondsRemaining(timeRemaining);
+        }
+    }, 1000);
+    
+    }
+    return(
+        <div>
+            <p>Countdown {seconds} seconds</p>
+            {!secondsRemaining&&(
+                <button type="button" onClick={onStart}>Start!</button>
+            )}
+        {secondsRemaining > 0 && (
+            <div>
+                {secondsRemaining} seconds remaining!
+            </div>
+
+        )}
+    
+            </div>
+    );
+
+}
+export default Countdown;
